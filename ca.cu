d8ae0d6b777208dc1c21e4a2669f5601ff15f431
Jordan <jordan@localhost.localdomain>
2017-04-23 14:30:50 +0100

    * Added printing to PNG * Code cleanup

--- ca.cu
+++ ca.cu
@@ -2,6 +2,10 @@
 #include <string>
 #include <math.h>
 #include <stdlib.h>
+#include <iterator> 
+
+#include <png++/png.hpp>
+
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 #include <thrust/iterator/counting_iterator.h>
@@ -9,14 +13,20 @@
 #include <thrust/iterator/zip_iterator.h>
 #include <thrust/for_each.h>
 
+// Random number in range functor
 struct randomInts{
 
+    private:
     const float a;
+    
+    public:
     randomInts(int A):a(A){}
+    
     __host__ __device__
     int operator()(){ return (int)std::floor((float)(a+1)*rand()/RAND_MAX); }
 };
 
+// CA Update process functor
 struct caUpdate{
     
     private:
@@ -36,25 +46,36 @@ struct caUpdate{
     }
 };
 
-void printer(thrust::host_vector<int>& x,std::string m){
+// Print thrust vector values to console
+template<class T>
+void consolePrinter(T& x, std::string m, const char* seperator)
+{
     std::cout << m;
-    thrust::copy(x.begin(),x.end(),std::ostream_iterator<int>(std::cout,"|"));
-    std::cout << std::endl;
+    thrust::copy(x.begin(),x.end(),std::ostream_iterator<int>(std::cout,seperator));
+    std::cout << "\n";
 }
 
-void printer(thrust::device_vector<int>& x,std::string m){
-    std::cout << m;
-    thrust::copy(x.begin(),x.end(),std::ostream_iterator<int>(std::cout,"|"));
-    std::cout << std::endl;
+// Print values to grayscale png
+template <class T,class I>
+void pngPrinter(T& x,int n, I& image)
+{   
+    thrust::copy(x.begin(),x.end(),image[n].begin());
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
+    
+    int range = 1;              // Cell neighbour view range
+    int length = atoi(argv[1]); // Length of cell array
+    int rule=atoi(argv[2]);     // Rule number
+    int steps=atoi(argv[3]);    // Number of update steps
 
-    // Host vector if random ints [0,range]
-    int range = 1, length = atoi(argv[1]), rule=atoi(argv[2]), steps=atoi(argv(3));
+    // PNG image storage    
+    png::image< png::gray_pixel > image(length,steps);
+
+    // Generate random initial cell state 
     thrust::host_vector<int> init(length);
     thrust::generate(init.begin(),init.end(),randomInts(range));
-    printer(init,"Initial state: ");
     
     // Ruleset matrix
     thrust::host_vector<int> rules(8);
@@ -64,28 +85,37 @@ int main(int argc, char* argv[]){
         rules[i] = x%(range+1);
         x = x >> 1;
     }
-    std::cout << "Ruleset " << rule << ": [";
-    thrust::copy(rules.begin(),rules.end(),std::ostream_iterator<int>(std::cout, "]["));
-    std::cout << std::endl;
-
-    // Maps to neighbours
+    consolePrinter(rules,"Ruleset:|","|");
+    
+    // Maps to neighbouring cells
     thrust::counting_iterator<int> it(0);
     thrust::device_vector<int> lft(length),rgt(length);
     lft[0] = length-1;
     thrust::copy(it,it+length,lft.begin()+1);
     thrust::copy(it+1,it+length,rgt.begin());
     rgt[length-1] = 0;
-    
+
+    // Front, back and rulest device vectors
     thrust::device_vector<int> bk(length),ft(length),d_rules(8);
     thrust::copy(init.begin(),init.end(),bk.begin());
     thrust::copy(rules.begin(),rules.end(),d_rules.begin());
 
+    // Colour transformation from states number to 8-bit grayscale
+    thrust::device_vector<int> clr(length);
+    thrust::fill(clr.begin(),clr.end(),255);
+
+    // Vector to copy to PNG image
+    thrust::device_vector<int> outVec(length);
+
+    // Initialize functor
     caUpdate CA(thrust::raw_pointer_cast(&d_rules[0]));
     
+    // Time step counter
     int counter = 0;
 
     while(counter < steps){
 
+        // Zip vectors and perform update from back to front
         thrust::for_each(
             thrust::make_zip_iterator(
                 thrust::make_tuple(
@@ -106,12 +136,17 @@ int main(int argc, char* argv[]){
             CA
         );
 
-        printer(ft,"step: ");
+        // Update colour value vector and copy to PNG
+        thrust::transform(ft.begin(),ft.end(),clr.begin(),outVec.begin(),thrust::multiplies<int>());
+        pngPrinter(outVec,counter,image);
+
+        // Swap front and back, increment
         ft.swap(bk);
         ++counter;
     }
     
-    
+    // Save PNG data
+    image.write("rule_"+std::string(argv[2])+".png");
 
     return 0;
 }
